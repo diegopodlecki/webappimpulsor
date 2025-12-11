1f2f8145b2fbcb7a493161a80e8d9efd3ce4fe45
Diego <diego.podlecki@gmail.com>
2025-12-10 21:15:55 -0300

    Primer comit de la web

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "Backend WebAppImpulsor funcionando correctamente"
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
